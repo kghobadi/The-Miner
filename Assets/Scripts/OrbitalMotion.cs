68d60d188226b0b1ec8d85bd22e1c9c047eafe39
Unknown <ksg304@nyu.edu>
2017-05-29 13:22:08 -0400

    Initial Commit with basic moving galaxy

---
+++ Assets/Scripts/OrbitalMotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitalMotion : MonoBehaviour {
+
+    public Transform sun;
+    public GameObject asteroid;
+
+    public float rotationSpeed;
+    public float orbitalSpeed;
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+        asteroid.transform.RotateAround(asteroid.transform.position, Vector3.forward, rotationSpeed);
+        asteroid.transform.RotateAround(sun.transform.position, Vector3.forward, rotationSpeed);
+		
+	}
+}
